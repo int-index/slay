16ac65b4ffd16a0f18e624dbb0e730baa8c6bf44
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-12-04 02:20:42 +0300

    Add slay-vty

---
+++ vty/src/Slay/Vty.hs
@@ -0,0 +1,11 @@
+module Slay.Vty
+  ( module Slay.Core,
+    module Slay.Vty.Prim,
+    module Slay.Vty.Inj,
+    module Slay.Vty.Render
+  ) where
+
+import Slay.Core
+import Slay.Vty.Prim
+import Slay.Vty.Inj
+import Slay.Vty.Render
---
+++ vty/src/Slay/Vty/Inj.hs
@@ -0,0 +1,18 @@
+module Slay.Vty.Inj
+  ( Inj(..)
+  ) where
+
+import Slay.Core
+
+class Inj p a where
+  inj :: p -> a
+
+instance Inj a a where
+  inj = id
+
+instance Inj p a => Inj p (Maybe a) where
+  inj = Just . inj
+
+instance (s -/ e, Inj p e) => Inj p (Collage s) where
+  inj = collageSingleton . inj
+
