1f3c69c61653a67fa4f02a0ac8e11ce34213fa1b
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-12-04 02:20:42 +0300

    Add slay-vty

---
+++ vty/src/Slay/Vty/Prim.hs
@@ -0,0 +1,49 @@
+module Slay.Vty.Prim
+  ( Prim(..),
+    image,
+    space,
+    string,
+    empty
+  ) where
+
+import qualified Graphics.Vty as Vty
+import Data.String
+
+import Slay.Core
+import Slay.Vty.Inj
+
+data Prim =
+  Prim
+    { primExtents :: Extents,
+      primImage :: Vty.Image
+    }
+  deriving (Eq, Show)
+
+image :: Inj Prim a => Vty.Image -> a
+image img = inj (Prim e img)
+  where
+    e = vtyImageExtents img
+
+space :: Inj Prim a => Extents -> a
+space e = inj (Prim e img)
+  where
+    Extents w h = e
+    img = Vty.backgroundFill (toSigned w) (toSigned h)
+
+string :: Inj Prim a => Vty.Attr -> String -> a
+string attr str = image (Vty.string attr str)
+
+empty :: Inj Prim a => a
+empty = inj (Prim e img)
+  where
+    e = Extents 0 0
+    img = Vty.emptyImage
+
+instance IsString Prim where
+  fromString = string Vty.defAttr
+
+vtyImageExtents :: Vty.Image -> Extents
+vtyImageExtents img =
+  Extents
+    { extentsW = unsafeToUnsigned (Vty.imageWidth img),
+      extentsH = unsafeToUnsigned (Vty.imageHeight img) }
---
+++ vty/src/Slay/Vty/Render.hs
@@ -0,0 +1,13 @@
+module Slay.Vty.Render
+  ( renderImageElements
+  ) where
+
+import Graphics.Vty as Vty
+import Slay.Core
+
+renderImageElements :: [(Offset, Image)] -> Picture
+renderImageElements [] = picForImage emptyImage
+renderImageElements els = picForLayers (renderElement <$> els)
+  where
+    renderElement (Offset{..}, image) =
+      translate offsetX offsetY image
