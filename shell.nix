846279be06d3177c682b5ce13e41e83f174c3b93
Vladislav Zavialov <vlad.z.4096@gmail.com>
2018-11-13 15:15:01 +0300

    Nix-based infra

--- shell.nix
+++ shell.nix
@@ -1,18 +1,25 @@
-# { stdenv, pkgs, haskell }:
-with import <nixpkgs> { };
+{ pkgs ? import <nixpkgs> {},
+  hc ? "ghc844"
+}:
 
-stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation rec {
   name = "slay";
   buildInputs = [
-    haskell.compiler.ghc844
+    pkgs.haskell.compiler.${hc}
     pkgs.zlib
     pkgs.cabal-install
     pkgs.pkgconfig
     pkgs.cairo
     pkgs.pango
     pkgs.gtk3
+    pkgs.which # for CI
   ];
   shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH
+    export LANG=en_US.UTF-8
   '';
+  LOCALE_ARCHIVE =
+    if pkgs.stdenv.isLinux
+    then "${pkgs.glibcLocales}/lib/locale/locale-archive"
+    else "";
 }
